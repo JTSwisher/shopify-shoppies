87191bf1ff6c0868a243f2a10fdd92e0018e83ed
JTSwisher <jefftswisher@gmail.com>
2020-12-16 21:47:10 -0700

    pass callback to Search component to set api results in parent App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
 import './App.css';
 import Search from './Search/Search'
 
 function App() {
+  const [movieData, setMovieData] = useState();
+
+  const setMovieDataCallback = (data) => {
+    setMovieData(data)
+  }
+
   return (
     <>
-      <Search />
+      <Search movieDataCallback={ setMovieDataCallback }/>
     </>
   );
 }
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 
-function Search() {
+function Search(props) {
     const [query, setQuery] = useState("");
     const [queryResults, setQueryResults] = useState();
     const API_KEY = process.env.REACT_APP_OMDB_KEY;
-    const axios = require('axios');
+    // const axios = require('axios');
 
     const fetchMovies = () => {
         fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${query}`)
         .then(res => res.json())
-        .then(result => console.log(result))
+        .then(result => setQueryResults(result.Search))
+        .then(props.movieDataCallback(queryResults))
     };
 
     const handleInputChange = (e) => {
