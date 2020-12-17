53325d9dc2c690849520097ff2e8a34f42801e32
JTSwisher <jefftswisher@gmail.com>
2020-12-16 21:33:36 -0700

    add function to handle input change, add function to fetch movie data based on input query

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 
 function Search() {
+    const [query, setQuery] = useState("");
+    const [queryResults, setQueryResults] = useState();
+    const API_KEY = process.env.REACT_APP_OMDB_KEY;
+    const axios = require('axios');
 
+    const fetchMovies = () => {
+        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${query}`)
+        .then(res => res.json())
+        .then(result => console.log(result))
+    };
+
+    const handleInputChange = (e) => {
+        setQuery(e);
+        fetchMovies()
+    };
 
     return(
         <div className="search-container">
-           
+            <form>
+                <input value={query} onChange={(e) => handleInputChange(e.target.value) } />
+            </form>
         </div>
     )
 };
